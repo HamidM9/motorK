d99cf7e6eb68b800db1e10e386846ef805cd710f
Hamid <hamidqpi@gmail.com>
2023-09-05 21:50:48 +0200

    Table Scetched

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-
+import "./Table.js"
+import MyTable from './Table.js';
 function App() {
   return (
     <div className="App">
@@ -8,7 +9,9 @@ function App() {
         <p>
           Initialize
         </p>
-        
+        <h1>Table for Cars</h1>
+        <MyTable></MyTable>
+        <MyTable></MyTable>
       </header>
     </div>
   );
---
+++ src/Table.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function MyTable() {
+  // Sample data with 20 entries
+
+
+  return (
+    <div>
+      
+      <table>
+        <thead>
+          <tr>
+            
+            <th>Make</th>
+            <th>Model</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default MyTable;
